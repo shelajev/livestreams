a04a9cb19c6bbd4105384be0d726bf64527d192f
Oleg Šelajev <oleg.selajev@oracle.com>
2021-03-03 15:30:16 +0000

    Autocommit "2021-03-03_15:30:16"

--- simplewebservice/src/main/java/simplewebservice/MainController.java
+++ simplewebservice/src/main/java/simplewebservice/MainController.java
@@ -1,2 +1,13 @@
-package simplewebservice;public class MainController {
+package simplewebservice;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/")
+public class MainController {
+
+  @Get("/hello/{name}")
+  public String hello(String name) {
+    return "Hello " + name + "!";
+  }
 }
