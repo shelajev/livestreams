db6271df1179247ba6b5963ea2700906c69624a0
Oleg Šelajev <oleg.selajev@oracle.com>
2021-03-03 15:23:08 +0000

    Autocommit "2021-03-03_15:23:08"

--- simplewebservice/src/main/java/simplewebservice/Application.java
+++ simplewebservice/src/main/java/simplewebservice/Application.java
@@ -5,6 +5,8 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello twitch !");
         Micronaut.run(Application.class, args);
     }
 }
