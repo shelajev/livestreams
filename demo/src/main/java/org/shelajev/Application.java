9485f1a95d50b3bef14294ad7df825f2d56129e0
Oleg Šelajev <oleg.selajev@oracle.com>
2021-03-23 16:29:31 +0000

    Autocommit "2021-03-23_16:29:30"

--- demo/src/main/java/org/shelajev/Application.java
+++ demo/src/main/java/org/shelajev/Application.java
@@ -1,7 +1,14 @@
 package org.shelajev;
 
 import io.micronaut.runtime.Micronaut;
-
+@KubernetesApplication(
+  name="demo", 
+  ports = @Port(name = "http", containerPort = 8080),
+  imagePullPolicy = ImagePullPolicy.Always
+  livenessProbe = @Probe(httpActionPath = "/health/liveness", initialDelaySeconds = 5),
+  readinessProbe = @Probe(httpActionPath = "/health/readiness", initialDelaySeconds = 5)
+)
+@DockerBuild(group = "olegshelajev", name ="demo")
 public class Application {
 
     public static void main(String[] args) {
