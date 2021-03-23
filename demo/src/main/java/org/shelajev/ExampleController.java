eea9e7c075cdd00d0fc46ced4a14ca2158e90290
Oleg Šelajev <oleg.selajev@oracle.com>
2021-03-23 16:48:07 +0000

    Autocommit "2021-03-23_16:48:07"

--- demo/src/main/java/org/shelajev/ExampleController.java
+++ demo/src/main/java/org/shelajev/ExampleController.java
@@ -1,9 +1,20 @@
 package org.shelajev;
 
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.http.annotation.*;
+
+import javax.inject.Inject;
 
 @Controller("/example")
 public class ExampleController {
+
+    @Inject
+    ApplicationContext context;
+
+    @Get("/config/{key}")
+    public String config(String key) {
+        return context.get(key, String.class).orElse("NOTHING");
+    }
 
     @Get(uri="/", produces="text/plain")
     public String index() {
