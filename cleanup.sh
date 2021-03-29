62cb70d08afa813f4e240bbdbcdddff4331486d1
Oleg Šelajev <oleg.selajev@oracle.com>
2021-03-29 12:00:03 +0000

    clean the main branch too

--- cleanup.sh
+++ cleanup.sh
@@ -7,5 +7,10 @@ git checkout -b $NAME
 git commit -am "Storing everything to a branch: $NAME"
 git push -u origin $NAME
 git checkout main
-
+#cleanup the main bran
+shopt -s extglob
+rm -rf -v !("LICENSE"|"README.MD"|"cleanup.sh")
+git add . 
+git commit -am "cleanup"
+shopt -u extglob
 
