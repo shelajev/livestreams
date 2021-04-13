851e6af27c766e18c653b7aafa13635d966767e7
Oleg Šelajev <oleg.selajev@oracle.com>
2021-04-13 16:33:45 +0000

    Autocommit "2021-04-13_16:33:45"

--- bare-se/src/main/java/me/opc/se/bare/Main2.java
+++ bare-se/src/main/java/me/opc/se/bare/Main2.java
@@ -2,13 +2,18 @@ package me.opc.se.bare;
 
 import org.graalvm.polyglot.Context;
 
+import java.util.function.Consumer;
+
 public class Main2 {
   public static void main(String[] args) {
     Context ctx = Context.create("js");
 
     var x = ctx.eval("js", "new Promise((resolve, reject) => {" +
-      "reject(42);" +
-      "}).then(console.log).catch(console.log)");
+      "resolve(42);" +
+      "})");
+
+    x.invokeMember("then",
+      (Consumer<Object>) (inside -> System.out.println("Java +" + inside)));
 
   }
 }
