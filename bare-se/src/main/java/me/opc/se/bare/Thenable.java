b8fa563d9de9a9592789d3ac267b6d530f92e1bc
Oleg Šelajev <oleg.selajev@oracle.com>
2021-04-13 18:17:36 +0000

    cleanup

--- bare-se/src/main/java/me/opc/se/bare/Thenable.java
+++
@@ -1,8 +0,0 @@
-package me.opc.se.bare;
-
-import org.graalvm.polyglot.Value;
-
-@FunctionalInterface
-public interface Thenable {
-  void then(Value resolve, Value reject);
-}
--- bare-se/src/main/java/me/opc/se/bare/package-info.java
+++
@@ -1,2 +0,0 @@
-
-package me.opc.se.bare;
